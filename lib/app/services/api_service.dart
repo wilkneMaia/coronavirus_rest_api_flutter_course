a5ecf801e0c699c712e874ac19a115f9673f3a18
Wilkne <wilkne@outlook.com>
2021-05-30 22:06:26 -0300

    feat(api_service.dart): created

---
+++ lib/app/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
+
+class APIService {
+  APIService(this.api);
+  final API api;
+
+  Future<String> getAccessToken() async {
+    final response = await http.post(
+      api.tokenUri(),
+      headers: {'Authorization': 'Basic ${api.apiKey}'},
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+      'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}',
+    );
+    throw response;
+  }
+}
