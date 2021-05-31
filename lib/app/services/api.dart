fdc314030ff2fc4d6c1c84383c236a7e409d0fd9
Wilkne <wilkne@outlook.com>
2021-05-30 23:06:15 -0300

    feat(api.dart | api_service.dart | /main.dart): refactoring

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,14 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
 
+enum EndPoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -14,4 +22,18 @@ class API {
         host: host,
         path: 'token',
       );
+
+  Uri endpointUri(EndPoint endPoint) => Uri(
+        scheme: 'https',
+        host: host,
+        path: _paths[endPoint],
+      );
+
+  static Map<EndPoint, String> _paths = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesSuspected: 'casesSuspected',
+    EndPoint.casesConfirmed: 'casesConfirmed',
+    EndPoint.deaths: 'deaths',
+    EndPoint.recovered: 'recovered',
+  };
 }
